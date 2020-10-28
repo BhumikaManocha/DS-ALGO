134b1da64dd2158ae29dcc61ab8a6caed4f357cb
ShivangiRoy <shivangi22.sr@gmail.com>
2020-10-28 19:44:15 +0530

    Fizz Buzz problem added

---
+++ GFG_InterviewPreparation/Arrays/Problems/FizzBuzz.cpp
@@ -0,0 +1,25 @@
+/*
+Input a number.
+Output numbers i from 1 to n. Print "Fizz" if i%3==0, 
+"Buzz" if i%5==0, "FizzBuzz" if i%15==0 else print i.
+*/
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> res;
+        
+        for(int i=1;i<=n;i++){
+            if(i%15==0)
+                res.push_back("FizzBuzz");
+            else if(i%3==0)
+                res.push_back("Fizz");
+            else if(i%5==0)
+                res.push_back("Buzz");
+            
+            else
+                res.push_back(to_string(i));
+            
+        }
+        return res;
+    }
+};
